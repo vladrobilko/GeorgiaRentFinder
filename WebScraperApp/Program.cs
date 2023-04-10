9b673ad2598d3099fb493253f2c621aea30efec0
vladrobilko <vladrobilko@gmail.com>
2023-04-10 19:22:42 +0400

    get images

--- WebScraperApp/Program.cs
+++ WebScraperApp/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
+using System.Security.Policy;
+using HtmlAgilityPack;
 
 namespace WebScraperApp
 {
@@ -7,6 +11,25 @@ namespace WebScraperApp
     {
         static void Main(string[] args)
         {
+            HtmlWeb web = new HtmlWeb();
+
+            var doc = web.Load("https://ss.ge/en/real-estate/3-room-flat-for-rent-kobuleti-5190457");
+
+
+
+            var urls = doc.DocumentNode.Descendants("img")
+                .Where(e => e.Attributes["class"]?.Value == "img-responsive")
+                .Select(e => e.GetAttributeValue("src", null))
+                .Where(s => !String.IsNullOrEmpty(s) && !s.Contains("Thumb") && s.Contains("static.ss.ge"))
+                .ToList();
+            /*
+             * var urls = document.DocumentNode.Descendants("img")
+                                .Where(e => e.Attributes["class"]?.Value == "my-class" 
+                                            && !string.IsNullOrEmpty(e.Attributes["alt"]?.Value))
+                                .Select(e => e.GetAttributeValue("src", null));
+             */
+
+
             var htmlScraper = new ScraperSsDotGe();
 
             var urlPageOne = "https://ss.ge/en/real-estate/l/For-Rent?Page=1&RealEstateDealTypeId=1&MunicipalityId=3&CityIdList=14&PriceType=false&CurrencyId=1";
@@ -22,7 +45,7 @@ namespace WebScraperApp
                                       $" \n\t Date: {item.Data}");
                 }
             });
-            
+
             var urlPageTwo = "https://ss.ge/en/real-estate/l/For-Rent?Page=2&RealEstateDealTypeId=1&MunicipalityId=3&CityIdList=14&PriceType=false&CurrencyId=1";
 
             var apartmentPageOTwo = htmlScraper.GetInfos(urlPageTwo);
--- WebScraperApp/ScraperSsDotGe.cs
+++ WebScraperApp/ScraperSsDotGe.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Xml.Linq;
 using HtmlAgilityPack;
 
 namespace WebScraperApp
@@ -9,7 +11,6 @@ namespace WebScraperApp
     {
         public List<Info> GetInfos(string url)
         {
-            var uri = new Uri(url);
             List<Info> infos = new List<Info>();
 
             HtmlWeb web = new HtmlWeb();
@@ -33,8 +34,49 @@ namespace WebScraperApp
                     continue;
                 }
             }
+ 
 
             return infos;
         }
+
+        public List<string> GetImages(HtmlDocument doc)
+        {
+            var images = new List<string>();
+
+            var input = doc.DocumentNode.SelectSingleNode("")?.InnerText;
+
+
+            return images;
+        }
+        //////*[@id="list"]/div[5]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div/div[1]/a - href
+        //1 //*[@id="list"]/div[5]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //////*[@id="list"]/div[5]/div[1]/div[1]/div[1]/div[2]/div[1]/a/div/span - title
+        //////*[@id="list"]/div[5]/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[2]/div/div[1]/text() - date
+        //
+        //////*[@id="list"]/div[6]/div[1]/div[1]/div[1]/div[1]/div/div[1]/div/div[1]/a/img
+        //2 //*[@id="list"]/div[6]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //////*[@id="list"]/div[6]/div[1]/div[1]/div[1]/div[2]/div[1]/a/div/span
+        //////*[@id="list"]/div[6]/div[1]/div[1]/div[1]/div[2]/div[2]/div/div[2]/div/div[1]/text() - date
+        //3 //*[@id="list"]/div[7]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //4 //*[@id="list"]/div[10]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //////*[@id="list"]/div[10]/div[1]/div[1]/div[1]/div[1]/div/a/img
+        /// 
+        //5 //*[@id="list"]/div[11]/div[1]/div[1]/div[2]/div[1]/div - по соглашению
+        /// //*[@id="list"]/div[12]/div[1]/div[1]/div[1]/div[2]/div[1]/a/div/span
+        //6 //*[@id="list"]/div[12]/div[1]/div[1]/div[2]/div[2]/div - по соглашению
+        //7 //*[@id="list"]/div[15]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //8 //*[@id="list"]/div[16]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //9 //*[@id="list"]/div[17]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //10//*[@id="list"]/div[20]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //11//*[@id="list"]/div[21]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //12//*[@id="list"]/div[22]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //13//*[@id="list"]/div[25]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //14//*[@id="list"]/div[26]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //15//*[@id="list"]/div[27]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //16//*[@id="list"]/div[28]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //17//*[@id="list"]/div[29]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //18//*[@id="list"]/div[30]/div[1]/div[1]/div[2]/div[2]/div - по соглашению
+        //19//*[@id="list"]/div[31]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
+        //20//*[@id="list"]/div[32]/div[1]/div[1]/div[2]/div[2]/div[1]/text()
     }
 }
