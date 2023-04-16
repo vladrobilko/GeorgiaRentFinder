a2062edf886158f956b6fe641406b518091c725a
vladrobilko <vladrobilko@gmail.com>
2023-04-16 17:44:41 +0400

    delete

--- TelegramBotApi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TelegramBotApi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
