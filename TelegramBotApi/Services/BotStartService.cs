4ab1c6a63ab9b3935f7cda1287915d42e148851b
vladrobilko <vladrobilko@gmail.com>
2023-04-19 16:54:48 +0400

    add bot start service

---
+++ TelegramBotApi/Services/BotStartService.cs
@@ -0,0 +1,36 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramBotApi.Services
+{
+    public class BotStartService : IHostedService
+    {
+        private readonly ITelegramBotClient _botClient;
+        private readonly IConfiguration _configuration;
+
+        public BotStartService(ITelegramBotClient telegramBotClient, IConfiguration configuration)
+        {
+            _botClient = telegramBotClient;
+            _configuration = configuration;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var usage = "______________________________________\n" +
+                              $"The bot started at {DateTime.UtcNow:dd/MM/yyyy HH:mm}\n" +
+                              "Usage:\n" +
+                              "/post";
+
+            return _botClient.SendTextMessageAsync(
+                chatId: _configuration.GetSection("BotConfiguration")["BotId"],
+                text: usage,
+                replyMarkup: new ReplyKeyboardRemove(),
+                cancellationToken: cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+    }
+}
