6b97fe9fe0813941789578a76954a0f3c5f53aa8
vladrobilko <vladrobilko@gmail.com>
2023-05-09 21:41:18 +0400

    add kutaisi link

---
+++ WebScraper/SsDotGe/ImeretiMunicipallyLinksSsDotGe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScraper.SsDotGe
+{
+    public static class ImeretiMunicipallyLinksSsDotGe
+    {
+        public static string GetKutaisiLink(int pageNumber)
+        {
+
+        }
+    }
+}
