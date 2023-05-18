618d206ad187c4c6bdd83c1e5ecd96d6a80b6878
vladrobilko <vladrobilko@gmail.com>
2023-05-18 17:31:51 +0400

    refactor

--- Application/Converters/FlatInfoClientModelConverter.cs
+++ Application/Converters/FlatInfoClientModelConverter.cs
@@ -13,20 +13,20 @@ namespace Application.Converters
         {
             var caption = GetCaptionWithOutCoordinateAndRealtor(flat);
 
-            if (IsCoordinateExist(flat.FlatCoordinateClientModel)) caption += GetCoordinateOrEmptyDescription(flat);
+            if (IsCoordinateExist(flat.FlatCoordinateClientModel)) caption += GetCoordinateOrEmptyDescribe(flat);
 
-            if (IsItRealtor(flat)) caption += GetRealtorDescription(flat.FlatPhoneClientModel.MentionOnSite);
+            if (IsItRealtor(flat)) caption += GetRealtorDescribe(flat.FlatPhoneClientModel.MentionOnSite);
 
             return caption;
         }
 
-        public static string ToTelegramCaptionWithRussianLanguage(this FlatInfoClientModel flat,bool isForAdmin, string language = null, string apiToken = null)
+        public static string ToTelegramCaptionWithRussianLanguage(this FlatInfoClientModel flat, bool isForAdmin, string language = null, string apiToken = null)
         {
             var caption = GetCaptionWithOutCoordinateAndRealtor(flat, language, apiToken);
 
-            if (IsCoordinateExist(flat.FlatCoordinateClientModel)) caption += GetCoordinateOrEmptyDescription(flat,"ru");
+            if (IsCoordinateExist(flat.FlatCoordinateClientModel)) caption += GetCoordinateOrEmptyDescribe(flat, "ru");
 
-            if (IsItRealtor(flat)) caption += GetRealtorDescription(flat.FlatPhoneClientModel.MentionOnSite,"ru");
+            if (IsItRealtor(flat)) caption += GetRealtorDescribe(flat.FlatPhoneClientModel.MentionOnSite, "ru");
 
             if (isForAdmin) caption += $"\nID in database - {flat.Id}";
 
@@ -39,11 +39,11 @@ namespace Application.Converters
             {
                 return $"{flat.Title.Translate(language, apiToken)}\n\n" +
 
-                       $"<strong>Цена:</strong> {flat.Cost} $ {GetCostInGelOrEmptyDescription(flat.Cost)}\n\n" +
+                       $"<strong>Цена:</strong> {flat.Cost} $ {GetCostInGelOrEmptyDescribe(flat.Cost)}\n\n" +
 
                        $"<strong>Просмотры на сайте:</strong> {flat.ViewsOnSite}\n" +
                        $"<strong>Опубликовано:</strong> {flat.SitePublication.ToCommonViewString()}\n" +
-                       $"<strong>Описание:</strong> {Regex.Replace(flat.Description.Translate(language, apiToken), @"\s{2,}", " ")}\n\n" +
+                       $"{GetDescriptionOrEmptyString(flat.Description, language, apiToken)}" +
 
                        $"<strong>Сайт:</strong><a href=\"{flat.PageLink}\"> link</a>\n" +
                        $"<strong>Телефон:</strong> {flat.FlatPhoneClientModel.PhoneNumber.Translate(language, apiToken)}\n";
@@ -51,17 +51,27 @@ namespace Application.Converters
 
             return $"{flat.Title}\n\n" +
 
-                 $"<strong>Cost:</strong> {flat.Cost} $ {GetCostInGelOrEmptyDescription(flat.Cost)}\n\n" +
+                 $"<strong>Cost:</strong> {flat.Cost} $ {GetCostInGelOrEmptyDescribe(flat.Cost)}\n\n" +
 
                  $"<strong>Views on site:</strong> {flat.ViewsOnSite}\n" +
                  $"<strong>Published:</strong> {flat.SitePublication.ToCommonViewString()}\n" +
-                 $"<strong>Description:</strong> {flat.Description}\n\n" +
+                 $"{GetDescriptionOrEmptyString(flat.Description)}" +
 
                  $"<strong>Web page:</strong><a href=\"{flat.PageLink}\"> link</a>\n" +
                  $"<strong>Mobile phone:</strong> {flat.FlatPhoneClientModel.PhoneNumber}\n";
         }
 
-        private static string GetCostInGelOrEmptyDescription(long cost)
+        private static string GetDescriptionOrEmptyString(string description, string language = null, string apiToken = null)
+        {
+            if (description == "No description") return "";
+
+            if (language == null) return $"<strong>Description:</strong> {description}\n\n";
+
+            return
+                $"<strong>Описание:</strong> {Regex.Replace(description.Translate(language, apiToken), @"\s{2,}", " ")}\n\n";
+        }
+
+        private static string GetCostInGelOrEmptyDescribe(long cost)
         {
             var usdToGel = UsdToGelCourseScraper.GetGelInOneDollarFromGeorgiaNationalBank();
 
@@ -70,7 +80,7 @@ namespace Application.Converters
             return $"({Convert.ToInt32(usdToGel * cost)} ლ)";
         }
 
-        private static string GetCoordinateOrEmptyDescription(FlatInfoClientModel flat, string language = null)
+        private static string GetCoordinateOrEmptyDescribe(FlatInfoClientModel flat, string language = null)
         {
             if (language == "ru")
             {
@@ -98,7 +108,7 @@ namespace Application.Converters
                    flat.FlatPhoneClientModel.PhoneNumber != "No number";
         }
 
-        private static string GetRealtorDescription(long mentionOnSite, string language = null)
+        private static string GetRealtorDescribe(long mentionOnSite, string language = null)
         {
             if (language == "ru")
             {
